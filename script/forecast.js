b58fa4ab92aff3c1999c9db5b5847b80e09ce0e4
Uciano <ansonuchenna@gmail.com>
2023-02-07 14:59:23 +0000

    Weather App

---
+++ script/forecast.js
@@ -0,0 +1,29 @@
+const key = 've0m16y1GKPZmdrq70pMGC15FafRX9QL';
+
+//Get Weather information
+const getWeather = async (id) =>  {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+
+    return data[0];
+
+};
+
+
+//Get City Information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0]
+}
+
+
+
